1415ac1b4625f0a12a8d0210c8e93d1c1f5b6efa
lisellelogan <liselle.logan@yahoo.com>
2021-06-24 18:04:06 -0400

    set up store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//redux
+import { createStore } from 'redux';
+
+let store = createStore()
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
