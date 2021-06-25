3c9d24777fe5fe950171729dd62f3181d84d5d72
lisellelogan <liselle.logan@yahoo.com>
2021-06-25 15:49:16 -0400

    create and build out UsersContainer and include in NavBarLanding

--- src/components/NavBarLanding.js
+++ src/components/NavBarLanding.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
+import UsersContainer from '../containers/UsersContainer';
 
 const NavBarLanding = () => {
     return(
     <div>
         <h1>Welcome to The Social Pup in App</h1>
-        <Link to="/users">All Dogs</Link>
+        <Link to="/users">All Users</Link>
 
         <Switch>
             <Route path="/users">
-                <p>Here are all the users</p>
+                <UsersContainer />
             </Route>
         </Switch>
     </div>
