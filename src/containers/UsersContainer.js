5c31318d859de3b66cbc184765a92247ec92f9ac
lisellelogan <liselle.logan@yahoo.com>
2021-06-25 16:08:08 -0400

    use connect to connect to store

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class UsersContainer extends Component {
     render(){
@@ -10,4 +11,4 @@ class UsersContainer extends Component {
     }
 }
 
-export default UsersContainer
+export default connect()(UsersContainer);
