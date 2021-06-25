8ea90f5a008df9e4b434bf70088685ede303b8ac
lisellelogan <liselle.logan@yahoo.com>
2021-06-25 16:35:25 -0400

    add outline for signup form

--- src/components/NavBarLanding.js
+++ src/components/NavBarLanding.js
@@ -4,11 +4,16 @@ import { Link, Switch, Route } from 'react-router-dom';
 const NavBarLanding = () => {
     return(
     <div>
-        <Link to="/login">Login</Link>
+        <Link to="/login">Login</Link><br/>
+        <Link to="/signup">Signup</Link>
 
         <Switch>
             <Route path="/login">
                 <p>Login Form</p>
+            </Route>
+
+            <Route path="/signup">
+                <p>Signup Form</p>
             </Route>
         </Switch>
     </div>
