5e1fcb19a232d18d4c4a2c94ee87f0fd86e84364
lisellelogan <liselle.logan@yahoo.com>
2021-06-25 14:54:38 -0400

    test switch

--- src/components/NavBarLanding.js
+++ src/components/NavBarLanding.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Switch, Route } from 'react-router-dom';
 
 const NavBarLanding = () => {
     return(
     <div>
         <h1>Welcome to The Social Pup in App</h1>
         <Link to="/users">All Dogs</Link>
+
+        <Switch>
+            <Route path="/users">
+                <p>Here are all the users</p>
+            </Route>
+        </Switch>
     </div>
     )
 }
