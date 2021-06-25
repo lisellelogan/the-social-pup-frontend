85056e5fcc2d0ece0a30669e9ac307d1e49d07c4
lisellelogan <liselle.logan@yahoo.com>
2021-06-25 16:11:15 -0400

    create LandingPageContainer

---
+++ src/containers/LandingPageContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LandingPageContainer extends Component {
+    render(){
+        return(
+            <div>
+                <h1>We are in the landing page container!</h1>
+            </div>
+        )
+    }
+}
+
+export default LandingPageContainer;
