789769081bbc4747e6e30d1f57177fe396ae17fc
lisellelogan <liselle.logan@yahoo.com>
2021-06-26 17:17:43 -0400

    add onChange to all inputs

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -26,15 +26,15 @@ class SignUpForm extends Component {
         return(
             <form>
                 <label>Email: </label> <input onChange={this.handleOnChange} type="text" name="email" value={this.state.email} placeholder="email@email.com"/><br/>
-                <label>Password: </label> <input type="text" name="password" value={this.state.password} /><br/>
-                <label>Pup Name: </label> <input type="text"name="pupName" value={this.state.pupName} /><br/> 
-                <label>Pup Age: </label> <input type="integer" name="pupAge" value={this.state.pupAge} /><br/> 
-                <label>Pup Breed: </label><input type="text" name="pupBreed" value={this.state.pupBreed} /><br/> 
+                <label>Password: </label> <input onChange={this.handleOnChange} type="text" name="password" value={this.state.password} /><br/>
+                <label>Pup Name: </label> <input onChange={this.handleOnChange} type="text"name="pupName" value={this.state.pupName} /><br/> 
+                <label>Pup Age: </label> <input onChange={this.handleOnChange} type="integer" name="pupAge" value={this.state.pupAge} /><br/> 
+                <label>Pup Breed: </label><input onChange={this.handleOnChange} type="text" name="pupBreed" value={this.state.pupBreed} /><br/> 
                 <label>Pup Fully Vaccinated? </label><br/>
-                Yes: <input type="radio" name="pupFullyVaccinated" value="Yes" />
-                No: <input type="radio" name="pupFullyVaccinated" value="No" />
-                <label>Pup Personality: </label> <input type="text" name="pupPersonality" value={this.state.pupPersonality} /><br/>
-                <label>Profile Picture: </label><input type="text" name="picture" value={this.state.picture} placeholder="image link" />
+                Yes: <input onChange={this.handleOnChange} type="radio" name="pupFullyVaccinated" value="Yes" />
+                No: <input onChange={this.handleOnChange} type="radio" name="pupFullyVaccinated" value="No" />
+                <label>Pup Personality: </label> <input onChange={this.handleOnChange} type="text" name="pupPersonality" value={this.state.pupPersonality} /><br/>
+                <label>Profile Picture: </label><input onChange={this.handleOnChange} type="text" name="picture" value={this.state.picture} placeholder="image link" />
             </form>
         )
     }
