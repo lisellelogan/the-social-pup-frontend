50e0889007d053072439d33dfbee4f1e5fa39ffe
lisellelogan <liselle.logan@yahoo.com>
2021-06-26 12:01:40 -0400

    add user key in rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import usersReducer from "./usersReducer";
 
 const rootReducer = combineReducers({
-    usersReducer
+    users: usersReducer
 })
 
 export default rootReducer
