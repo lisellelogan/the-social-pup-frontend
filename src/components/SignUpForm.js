1ccde61b790519193e1dd2009506a6c7fdb0012b
lisellelogan <liselle.logan@yahoo.com>
2021-06-26 15:52:15 -0400

    build out signup form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
 class SignUpForm extends Component {
+
+    state = {
+        email: "",
+        password: "",
+        pup_name: "",
+        pup_age: "",
+        pup_breed: "",
+        pup_fully_vaccinated: false,
+        pup_personality: "",
+        owner_name: "",
+        picture: ""
+    }
+
     render(){
         return(
-            <h1>SingUp Form</h1>
+            <form>
+                <input type="text" name="email" value={this.state.email}/>
+                <input type="text" name="password" value={this.state.password} />
+                <input type="text"name="pup_name" value{this} />
+                <input />
+                <input />
+                <input />
+                <input />
+                <input />
+                <input />
+            </form>
         )
     }
 }
