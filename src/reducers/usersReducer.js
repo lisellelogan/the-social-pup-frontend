4d73030a43a49a52f44dcb7ec38664d8bf55f44d
lisellelogan <liselle.logan@yahoo.com>
2021-06-26 12:49:02 -0400

    change state to an array since using combineReducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,12 +1,7 @@
-export default function usersReducer(state={logged_in: false, users: [], friends: []}, action) {
+export default function usersReducer(state = [], action) {
     switch(action.type){
         case "GOT_USERS":
-            return {
-                ...state,
-                logged_in: true,
-                users: action.payload,
-                friends: []
-            }
+            return action.payload
         default:
             return state
     }
