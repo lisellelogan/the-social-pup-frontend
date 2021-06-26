f19eec42cc3ec988a9d662384f3ac2a5435662de
lisellelogan <liselle.logan@yahoo.com>
2021-06-26 13:21:02 -0400

    rearrange User Card

--- src/components/User.js
+++ src/components/User.js
@@ -4,14 +4,14 @@ const User = (props) => {
     const user = props.user.attributes
     return(
         <li>
-            <img src={user.picture} alt={user.picture}/>
-            Email: {user.email}<br/>
+            <img src={user.picture} alt={user.picture}/><br/>
             Pup Name: {user.pup_name}<br/>
             Pup Age: {user.pup_age}<br/>
             Pup Breed: {user.pup_breed}<br/>
             Pup Fully Vaccinated: {user.pup_fully_vaccinated ? "Yes" : "No"}<br/>
             Pup Personality: {user.pup_personality}<br/>
             Owner Name: {user.owner_name}<br/>
+            Email: {user.email}<br/><br/>
         </li>
     )
 }
