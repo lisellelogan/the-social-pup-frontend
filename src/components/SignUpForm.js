6871c652edeb5b9e8f6ce3113d0cd1c5d574497e
lisellelogan <liselle.logan@yahoo.com>
2021-06-26 16:30:44 -0400

    use placeholders in signup form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -17,16 +17,16 @@ class SignUpForm extends Component {
     render(){
         return(
             <form>
-                <label>Email: </label> <input type="text" name="email" value={this.state.email}/><br/>
+                <label>Email: </label> <input type="text" name="email" value={this.state.email} placeholder="email@email.com"/><br/>
                 <label>Password: </label> <input type="text" name="password" value={this.state.password} /><br/>
                 <label>Pup Name: </label> <input type="text"name="pupName" value={this.state.pupName} /><br/> 
                 <label>Pup Age: </label> <input type="integer" name="pupAge" value={this.state.pupAge} /><br/> 
                 <label>Pup Breed: </label><input type="text" name="pupBreed" value={this.state.pupBreed} /><br/> 
                 <label>Pup Fully Vaccinated? </label><br/>
-                Yes: <input type="checkbox" name="pupIsFullyVaccinated"/>
-                No: <input type="checkbox" name="pupIsNotFullyVaccinated"/>
-                <input />
-                <input />
+                Yes: <input type="radio" name="pupFullyVaccinated" value="Yes" />
+                No: <input type="radio" name="pupFullyVaccinated" value="No" />
+                <label>Pup Personality: </label> <input type="text" name="pupPersonality" value={this.state.pupPersonality} /><br/>
+                <label>Profile Picture: </label><input type="text" name="picture" value={this.state.picture} placeholder="image link" />
             </form>
         )
     }
