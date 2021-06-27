81db96ad4d907f9a2387eb22b71213550ba9513b
lisellelogan <liselle.logan@yahoo.com>
2021-06-27 11:07:16 -0400

    create fetch request for create user

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,6 +1,6 @@
 const url = "http://127.0.0.1:3001/users"
 
-export const setUsers = (users) => ({type: "GOT_USERS", payload: users})
+const setUsers = (users) => ({type: "GOT_USERS", payload: users})
 
 export const fetchUsers = () => {
     return (dispatch) => {
@@ -9,6 +9,26 @@ export const fetchUsers = () => {
         .then(data => {
             const users = data.data
             dispatch(setUsers(users))
+        })
+    }
+}
+
+export const createUser = (user) => {
+    return (dispatch) => {
+        const configObj = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(user)
+        }
+
+        fetch(url, configObj)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+
         })
     }
 }
