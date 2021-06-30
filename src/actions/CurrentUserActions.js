7c0a5d68b4dc6a6669c04fbfc030585f15a27120
lisellelogan <liselle.logan@yahoo.com>
2021-06-30 14:58:57 -0400

    comment out Login function for now

--- src/actions/CurrentUserActions.js
+++ src/actions/CurrentUserActions.js
@@ -1,14 +1,14 @@
 export const CurrentUserActions = (user) => ({type: "SET_CURRENT_USER", payload: user})
 
-export const Login = (userInput) => {
-    return (dispatch) => {
-        const configObj = {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json"
-            },
-            body: JSON.stringify(userInput)
-        }
-    }
-}
+// export const Login = (userInput) => {
+//     return (dispatch) => {
+//         const configObj = {
+//             method: 'POST',
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 Accept: "application/json"
+//             },
+//             body: JSON.stringify(userInput)
+//         }
+//     }
+// }
