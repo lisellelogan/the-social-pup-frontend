6ab5a5332301cfd009029b7563ead3ade8ef47e0
lisellelogan <liselle.logan@yahoo.com>
2021-06-30 15:51:36 -0400

    change function name and create default for state

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,4 +1,4 @@
-export default function userReducer(state = false, action) {
+export default function loginReducer(state = {logged_in: false, current_user: null}, action) {
     switch (action.type){
         case "SET_CURRENT_USER":
             return action.payload
