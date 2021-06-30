5d73d4044d62fa8edefb8b35872ce4f548ae8d9f
lisellelogan <liselle.logan@yahoo.com>
2021-06-30 14:15:36 -0400

    create current user action and set up function to make fetch request

---
+++ src/actions/CuurentUserActions.js
@@ -0,0 +1,7 @@
+export const CurrentUserActions = (user) => ({type: "SET_CURRENT_USER", payload: user})
+
+export const userLogin = (userInput) => {
+    return {
+        // fetch to backend to find or create by user
+    }
+}
