d2e66ba0fd9cf7ee3fe7a872e1e6c195f722a545
lisellelogan <liselle.logan@yahoo.com>
2021-07-01 11:49:43 -0400

    fix reducer to include loggedIn nad currentUser

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,7 +1,10 @@
 export default function loginReducer(state = {loggedIn: false, currentUser: null}, action) {
     switch (action.type){
         case "SET_CURRENT_USER":
-            return action.payload
+            return {
+                loggedIn: true,
+                currentUser: action.payload
+            }
         default:
             return state
     }
