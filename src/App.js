1dc02c052dba8a52b1cab1c63b4122fdf46eb92c
lisellelogan <liselle.logan@yahoo.com>
2021-07-01 14:03:09 -0400

    debug routes and comment out RestrictedNavBar for now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import LandingPageContainer from './containers/LandingPageContainer';
 import Welcome from './components/Welcome';
+import UsersContainer from './containers/UsersContainer';
+import Logout from './components/Logout';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-
 
 export default class App extends Component {
   render(){
@@ -11,6 +12,8 @@ export default class App extends Component {
       <>
       <Switch>
         <Route path="/welcome" exact component={Welcome} />
+        <Route path="/users" exact component={UsersContainer} />
+        <Route path="/logout" exact component={Logout} />
         <Route path ="/" component={LandingPageContainer} />
       </Switch>
       </>
