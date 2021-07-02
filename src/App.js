e12fa668a6557dda88f51e23d121b108aac2274f
lisellelogan <liselle.logan@yahoo.com>
2021-07-02 01:38:24 -0400

    debug conditional to render page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import LandingPageContainer from './containers/LandingPageContainer';
-// import Welcome from './components/Welcome';
-// import UsersContainer from './containers/UsersContainer';
-// import Logout from './components/Logout';
-// import { Switch, Route } from 'react-router-dom';
 import RestrictedNavBar from './components/RestrictedNavBar';
 import { connect } from 'react-redux';
 import './App.css';
@@ -16,17 +12,10 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  loggedIn: state.loggedIn.loggedIn
-})
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.loggedIn.loggedIn
+  }
+}
 
 export default connect(mapStateToProps)(App)
-
-// <>
-      {/* <Switch> */}
-        {/* <Route path="/welcome" exact component={Welcome} />
-        <Route path="/users" exact component={UsersContainer} />
-        <Route path="/logout" exact component={Logout} /> */}
-        {/* <Route path ="/" component={LandingPageContainer} />
-      </Switch> */}
-      // </>
