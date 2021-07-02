56e218086f3e65bbbed8c0a96e23a37645e885a2
lisellelogan <liselle.logan@yahoo.com>
2021-07-02 11:38:17 -0400

    make logout a form and logout current user

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { LogoutCurrentUser } from '../actions/CurrentUserActions';
 // import LandingPageContainer from '../containers/LandingPageContainer';
 
-const Logout = ({ LogoutCurrentUser }) => {
-    LogoutCurrentUser()
+const Logout = (props) => {
+    return (
+        <form onSubmit={props.LogoutCurrentUser()}>
+            <input type="submit" value="Logout" />
+        </form>
+    )
 }
 
 export default connect(null, { LogoutCurrentUser })(Logout);
