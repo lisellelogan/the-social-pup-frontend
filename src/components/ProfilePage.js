ea18197ad0b27ac8f7459af3509c8a33317a68c5
lisellelogan <liselle.logan@yahoo.com>
2021-07-02 14:07:10 -0400

    add user info to profile page

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -9,7 +9,17 @@ const ProfilePage = (props) => {
         //make friendsList component
     
     return(
-        <div>{props.currentUser.pup_name}</div>
+        <div>
+            <img src={props.currentUser.picture} alt={props.currentUser.picture}/><br/>
+            Pup Name: {props.currentUser.pup_name}<br/>
+            Pup Age: {props.currentUser.pup_age}<br/>
+            Pup Breed: {props.currentUser.pup_breed}<br/>
+            Pup Fully Vaccinated: {props.currentUser.pup_fully_vaccinated ? "Yes" : "No"}<br/>
+            Pup Personality: {props.currentUser.pup_personality}<br/>
+            Pup Location: {props.currentUser.pup_location}<br/>
+            Owner Name: {props.currentUser.owner_name}<br/>
+            Email: {props.currentUser.email}<br/><br/>
+        </div>
     )
     
 }
