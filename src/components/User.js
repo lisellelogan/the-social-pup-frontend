5e8a0a0ec7ffa4f0a4b0ac10f99c3bfa36f774b5
lisellelogan <liselle.logan@yahoo.com>
2021-07-02 18:32:50 -0400

    add love and nope button for friends

--- src/components/User.js
+++ src/components/User.js
@@ -3,17 +3,21 @@ import React from 'react';
 const User = (props) => {
     const user = props.user.attributes
     return(
-        <li>
-            <img src={user.picture} alt={user.picture}/><br/>
-            Pup Name: {user.pup_name}<br/>
-            Pup Age: {user.pup_age}<br/>
-            Pup Breed: {user.pup_breed}<br/>
-            Pup Fully Vaccinated: {user.pup_fully_vaccinated ? "Yes" : "No"}<br/>
-            Pup Personality: {user.pup_personality}<br/>
-            Pup Location: {user.pup_location}<br/>
-            Owner Name: {user.owner_name}<br/>
-            Email: {user.email}<br/><br/>
-        </li>
+        <div>
+            <li>
+                <img src={user.picture} alt={user.picture}/><br/>
+                Pup Name: {user.pup_name}<br/>
+                Pup Age: {user.pup_age}<br/>
+                Pup Breed: {user.pup_breed}<br/>
+                Pup Fully Vaccinated: {user.pup_fully_vaccinated ? "Yes" : "No"}<br/>
+                Pup Personality: {user.pup_personality}<br/>
+                Pup Location: {user.pup_location}<br/>
+                Owner Name: {user.owner_name}<br/>
+                Email: {user.email}<br/><br/>
+            </li>
+            <button>Love</button>  <button>Nope</button><br/><br/>
+        </div>
+        
     )
 }
 
