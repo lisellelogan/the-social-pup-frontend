8d895231a2175122c37db8c8138d6f1de1a926b2
lisellelogan <liselle.logan@yahoo.com>
2021-07-02 19:56:55 -0400

    figure out relationships to display for friends

---
+++ src/actions/FriendsActions.js
@@ -0,0 +1,14 @@
+const url = "http://127.0.0.1:3001/users"
+
+const getFriends = (friends) => ({type: "GOT_FRIENDS", payload: friends})
+
+export const fetchFriends = () => {
+    return (dispatch) => {
+        fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+            const users = data.data
+            users.map
+        })
+    }
+}
