12ae9f02e4e72c329ee1c9fc986e51ebc52d649e
lisellelogan <liselle.logan@yahoo.com>
2021-07-02 01:14:06 -0400

    test routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import LandingPageContainer from './containers/LandingPageContainer';
-import Welcome from './components/Welcome';
-import UsersContainer from './containers/UsersContainer';
-import Logout from './components/Logout';
-import { Switch, Route } from 'react-router-dom';
+// import Welcome from './components/Welcome';
+// import UsersContainer from './containers/UsersContainer';
+// import Logout from './components/Logout';
+// import { Switch, Route } from 'react-router-dom';
+import RestrictedNavBar from './components/RestrictedNavBar';
+import { connect } from 'react-redux';
 import './App.css';
 
-export default class App extends Component {
+class App extends Component {
   render(){
     return(
-      <>
-      <Switch>
-        <Route path="/welcome" exact component={Welcome} />
-        <Route path="/users" exact component={UsersContainer} />
-        <Route path="/logout" exact component={Logout} />
-        <Route path ="/" component={LandingPageContainer} />
-      </Switch>
-      </>
+      this.props.loggedIn ? <RestrictedNavBar /> : <LandingPageContainer />
     )
   }
 }
+
+const mapStateToProps = state => ({loggedIn: state.loggedIn })
+
+export default connect(mapStateToProps)(App);
+
+// <>
+      {/* <Switch> */}
+        {/* <Route path="/welcome" exact component={Welcome} />
+        <Route path="/users" exact component={UsersContainer} />
+        <Route path="/logout" exact component={Logout} /> */}
+        {/* <Route path ="/" component={LandingPageContainer} />
+      </Switch> */}
+      // </>
