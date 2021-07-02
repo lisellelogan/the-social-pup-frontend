022bd0a8592cfc9153a6c0b34e7537e0a14db9ef
lisellelogan <liselle.logan@yahoo.com>
2021-07-02 01:42:03 -0400

    comment in RestrictedNavBar

--- src/components/RestrictedNavBar.js
+++ src/components/RestrictedNavBar.js
@@ -1,19 +1,19 @@
-// import React from 'react';
-// import { Switch, Route } from 'react-router-dom';
-// import UsersContainer from '../containers/UsersContainer';
-// import Logout from './Logout';
-// import RestrictedNavBarLinks from './RestrictedNavBarLinks';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import UsersContainer from '../containers/UsersContainer';
+import Logout from './Logout';
+import RestrictedNavBarLinks from './RestrictedNavBarLinks';
 
-// const RestrictedNavBar = () => {
-//     return(
-//         <>
-//             <RestrictedNavBarLinks />
-//             <Switch>
-//                 <Route path="/users" exact component={UsersContainer} />
-//                 <Route path="/logout" exact component={Logout} />
-//             </Switch>
-//         </>
-//     )
-// }
+const RestrictedNavBar = () => {
+    return(
+        <>
+            <RestrictedNavBarLinks />
+            <Switch>
+                <Route path="/users" exact component={UsersContainer} />
+                <Route path="/logout" exact component={Logout} />
+            </Switch>
+        </>
+    )
+}
 
-// export default RestrictedNavBar;
+export default RestrictedNavBar;
