741c10a21b812958767bcc01e7155ae8f2128d46
lisellelogan <liselle.logan@yahoo.com>
2021-07-02 19:15:47 -0400

    add friendsReducer to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
+import friendsReducer from "./friendsReducer";
 import loginReducer from "./loginReducer";
 import usersReducer from "./usersReducer";
 
 const rootReducer = combineReducers({
     users: usersReducer,
-    loggedIn: loginReducer
+    loggedIn: loginReducer,
+    friends: friendsReducer
 })
 
 export default rootReducer
