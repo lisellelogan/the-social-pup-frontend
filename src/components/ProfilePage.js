4286b1de2b9123454b5698c6b2ea43cef97846bc
lisellelogan <liselle.logan@yahoo.com>
2021-07-02 12:24:34 -0400

    create profile page and add user

---
+++ src/components/ProfilePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import User from './User';
+
+const ProfilePage = () => {
+    //here i want to display user info
+        //user component
+    //display list of friends
+        //make friendsList component
+    return(
+        <User />
+    )
+    
+}
+
+export default ProfilePage;
