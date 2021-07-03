73e84e75c15679b03147c92a912286b0d5639a9a
lisellelogan <liselle.logan@yahoo.com>
2021-07-03 13:06:02 -0400

    make friend card

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Friend = (props) => {
     return (
-        <h1>{props.pup_name}</h1>
+        <div>
+            <li>
+                <img src={props.friend.picture} alt={props.friend.picture} /><br/>
+                Pup Name: {props.friend.pup_name}<br/>
+                Pup Age: {props.friend.pup_age}<br/>
+                Email: {props.friend.email}<br/>
+            </li>
+            {/* will add event listener to delete friend */}
+            <button>Remove Friend</button><br/><br/>
+        </div>
     )
 }
 
