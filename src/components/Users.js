c3eabccb4d58c06ec753b0cbe7339af8317c176c
lisellelogan <liselle.logan@yahoo.com>
2021-07-04 15:55:40 -0400

    remove comments

--- src/components/Users.js
+++ src/components/Users.js
@@ -11,35 +11,5 @@ const Users = (props) => {
         </ul>
     )
 }
-//     // only want to pass users that are not in friends list nad not a current user
-//     const friends = props.friends
-//     const users = props.users
-
-//     const filteredUsers = (users, friends) => {
-
-//         let filterUsers = users.filter(user => !friends.some(friend => (
-//             friend.id === user.id
-//             )
-//         ))
-//         return filterUsers
-//     }
-
-//     const filteredUsersList = filteredUsers(users, friends).map(user => props.currentUser.id !== user.id ? <User key={user.id} user={user} /> : [])
-
-//     return(
-//         <ul>
-//            {filteredUsersList}
-//         </ul>
-//     )
-// }
-
-// const mapStateToProps = (state) => {
-//     return {
-//         currentUser: state.loggedIn.currentUser.data.attributes,
-//         friends: state.loggedIn.currentUser.data.attributes.friends
-//     }
-// }
-
-// // props.currentUser.id === user.id ? <User key={user.id} user={user} /> : ""
 
 export default Users;
