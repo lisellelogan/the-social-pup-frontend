81e80ab533b54992524a16334576718b6c30de30
lisellelogan <liselle.logan@yahoo.com>
2021-07-04 13:53:13 -0400

    add spread operator for users

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,7 +1,7 @@
 export default function usersReducer(state = [], action) {
     switch(action.type){
         case "GOT_USERS":
-            return action.payload
+            return [...state, action.payload]
         case "ADDED_USER":
             return [
                 ...state,
