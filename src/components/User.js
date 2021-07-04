bda6d68b7c2794aaa963cd69514e703fc7d97c99
lisellelogan <liselle.logan@yahoo.com>
2021-07-04 10:20:00 -0400

    make mapsDispatchToProps to add another action to dispatch

--- src/components/User.js
+++ src/components/User.js
@@ -3,44 +3,37 @@ import { AddFriend } from '../actions/CurrentUserActions';
 import {connect } from 'react-redux';
 
 
-class User extends Component {
+const User = (props) => {
 
-    state = {
-        user: this.props.user
-    }
+    const user = props.user
 
-    render() {
+    const friend = props.currentUser.friends.filter(friend => props.user.id === friend.id)
 
-        const user = props.user
-
-        const friend = props.currentUser.friends.filter(friend => props.user.id === friend.id)
-    
-        const handleOnClick = event => {
-            if (event.target.innerText === "Love" && friend.length === 0) {
-                props.AddFriend(props.currentUser.id, props.user.id)
-            } else {
-                alert("You're already friends!")
-            }
+    const handleOnClick = event => {
+        if (event.target.innerText === "Love" && friend.length === 0) {
+            props.AddFriend(props.currentUser.id, props.user.id) && props.RemoveUserFromList(props.user.id)
+        } else {
+            alert("You're already friends!")
         }
-
-        return(
-            <div>
-                <li id={user.id}>
-                    <img src={user.picture} alt={user.picture}/><br/>
-                    Pup Name: {user.pup_name}<br/>
-                    Pup Age: {user.pup_age}<br/>
-                    Pup Breed: {user.pup_breed}<br/>
-                    Pup Fully Vaccinated: {user.pup_fully_vaccinated ? "Yes" : "No"}<br/>
-                    Pup Personality: {user.pup_personality}<br/>
-                    Pup Location: {user.pup_location}<br/>
-                    Owner Name: {user.owner_name}<br/>
-                    Email: {user.email}<br/><br/>
-                </li>
-                <button onClick={handleOnClick}>Love</button>  <button onClick={handleOnClick}>Nope</button><br/><br/>
-            </div>
-            
-        )
     }
+
+    return(
+        <div>
+            <li id={user.id}>
+                <img src={user.picture} alt={user.picture}/><br/>
+                Pup Name: {user.pup_name}<br/>
+                Pup Age: {user.pup_age}<br/>
+                Pup Breed: {user.pup_breed}<br/>
+                Pup Fully Vaccinated: {user.pup_fully_vaccinated ? "Yes" : "No"}<br/>
+                Pup Personality: {user.pup_personality}<br/>
+                Pup Location: {user.pup_location}<br/>
+                Owner Name: {user.owner_name}<br/>
+                Email: {user.email}<br/><br/>
+            </li>
+            <button onClick={handleOnClick}>Love</button>  <button onClick={handleOnClick}>Nope</button><br/><br/>
+        </div>
+        
+    )
 }
 
 const mapStateToProps = state => {
@@ -49,4 +42,11 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, {AddFriend})(User);
+const mapDispatchToProps = dispatch => {
+    return {
+        AddFriend: (currentUser, user) => dispatch(AddFriend(currentUser, user)),
+        RemoveUserFromList: (user) => dispatch(RemoveUserFromList(user))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(User);
