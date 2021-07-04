6fa702ced115b97db626b924953d621aa36eb09e
lisellelogan <liselle.logan@yahoo.com>
2021-07-04 15:38:13 -0400

    uncomment

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,45 +1,37 @@
 import React from 'react';
 import User from './User';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 
 const Users = (props) => {
-    const users = props.users.map(user => <User key={user.id} user={user}/>)
+    // only want to pass users that are not in friends list nad not a current user
+    const friends = props.friends
+    const users = props.users
 
-    return (
+    const filteredUsers = (users, friends) => {
+
+        let filterUsers = users.filter(user => !friends.some(friend => (
+            friend.id === user.id
+            )
+        ))
+        return filterUsers
+    }
+
+    const filteredUsersList = filteredUsers(users, friends).map(user => props.currentUser.id !== user.id ? <User key={user.id} user={user} /> : [])
+
+    return(
         <ul>
-            {users}
+           {filteredUsersList}
         </ul>
     )
 }
-//     // only want to pass users that are not in friends list nad not a current user
-//     const friends = props.friends
-//     const users = props.users
 
-//     const filteredUsers = (users, friends) => {
+const mapStateToProps = (state) => {
+    return {
+        currentUser: state.loggedIn.currentUser.data.attributes,
+        friends: state.loggedIn.currentUser.data.attributes.friends
+    }
+}
 
-//         let filterUsers = users.filter(user => !friends.some(friend => (
-//             friend.id === user.id
-//             )
-//         ))
-//         return filterUsers
-//     }
-
-//     const filteredUsersList = filteredUsers(users, friends).map(user => props.currentUser.id !== user.id ? <User key={user.id} user={user} /> : [])
-
-//     return(
-//         <ul>
-//            {filteredUsersList}
-//         </ul>
-//     )
-// }
-
-// const mapStateToProps = (state) => {
-//     return {
-//         currentUser: state.loggedIn.currentUser.data.attributes,
-//         friends: state.loggedIn.currentUser.data.attributes.friends
-//     }
-// }
-
-// // props.currentUser.id === user.id ? <User key={user.id} user={user} /> : ""
+// props.currentUser.id === user.id ? <User key={user.id} user={user} /> : ""
 
 export default Users;
