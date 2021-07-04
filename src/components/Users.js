f72c5e21a115fff9a9753e5345086db54764dc75
lisellelogan <liselle.logan@yahoo.com>
2021-07-04 17:04:00 -0400

    remove comments

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import User from './User';
-// import { connect } from 'react-redux';
 
 const Users = (props) => {
-    // const users = props.users.map(user => <User key={user.id} user={user} />)
     const users = props.users.map(user => props.currentUser.id !== user.id ? <User key={user.id} user={user} /> : [])
 
     return (
@@ -13,35 +11,4 @@ const Users = (props) => {
     )
 
 }
-//     // only want to pass users that are not in friends list nad not a current user
-//     const friends = props.friends
-//     const users = props.users
-
-//     const filteredUsers = (users, friends) => {
-
-//         let filterUsers = users.filter(user => !friends.some(friend => (
-//             friend.id === user.id
-//             )
-//         ))
-//         return filterUsers
-//     }
-
-//     const filteredUsersList = filteredUsers(users, friends).map(user => props.currentUser.id !== user.id ? <User key={user.id} user={user} /> : [])
-
-//     return(
-//         <ul>
-//            {filteredUsersList}
-//         </ul>
-//     )
-// }
-
-// const mapStateToProps = (state) => {
-//     return {
-//         currentUser: state.loggedIn.currentUser.data.attributes,
-//         friends: state.loggedIn.currentUser.data.attributes.friends
-//     }
-// }
-
-// // props.currentUser.id === user.id ? <User key={user.id} user={user} /> : ""
-
 export default Users;
