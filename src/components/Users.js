5105a50e212e9a0a6325f83460d78c3621924a2c
lisellelogan <liselle.logan@yahoo.com>
2021-07-03 22:31:44 -0400

    figure out filter for users

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import User from './User';
+import { connect } from 'react-redux';
 
 const Users = (props) => {
-    const users = props.users.map(
-        user => <User key={user.id} user={user} />
-    )
+    // only want to pass users that are not in friends list nad not a current user
+    const friends = props.friends
+
+    const users = props.users
+    
+    const filteredUsers = (friends, users) => {
+        const findFriends = [];
+
+        friends.forEach(friend => users.forEach(user => {
+            if (friend.id !== user.id){
+                findFriends.push(friend)
+            }
+        }))
+        return findFriends
+    }
+
+    debugger
+
+    // const users = props.users.map(
+    //     user => <User key={user.id} user={user} />
+    // )
 
     return(
         <ul>
-           { users }
+           { filteredUsers(friends, users) }
         </ul>
     )
 }
 
+const mapStateToProps = (state) => {
+    return {
+        currentUser: state.loggedIn.currentUser.data.attributes,
+        friends: state.loggedIn.currentUser.data.attributes.friends
+    }
+}
+
 // props.currentUser.id === user.id ? <User key={user.id} user={user} /> : ""
 
-export default Users;
+export default connect(mapStateToProps)(Users);
