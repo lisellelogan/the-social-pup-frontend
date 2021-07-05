6e38dd9ce4788a6e23e6a1327ecdd93b89f0f70b
lisellelogan <liselle.logan@yahoo.com>
2021-07-05 10:52:06 -0400

    Pre-styling commit

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -5,17 +5,16 @@ const ProfilePage = (props) => {
     
     return(
         <div>
-            <div>
-                <img src={props.currentUser.picture} alt={props.currentUser.picture}/><br/>
-                Pup Name: {props.currentUser.pup_name}<br/>
-                Pup Age: {props.currentUser.pup_age}<br/>
-                Pup Breed: {props.currentUser.pup_breed}<br/>
-                Pup Fully Vaccinated: {props.currentUser.pup_fully_vaccinated ? "Yes" : "No"}<br/>
-                Pup Personality: {props.currentUser.pup_personality}<br/>
-                Pup Location: {props.currentUser.pup_location}<br/>
-                Owner Name: {props.currentUser.owner_name}<br/>
-                Email: {props.currentUser.email}<br/><br/>
-            </div>
+            <h1>{props.currentUser.pup_name}'s Profile</h1>
+            <img src={props.currentUser.picture} alt={props.currentUser.picture}/><br/>
+            Pup Name: {props.currentUser.pup_name}<br/>
+            Pup Age: {props.currentUser.pup_age}<br/>
+            Pup Breed: {props.currentUser.pup_breed}<br/>
+            Pup Fully Vaccinated: {props.currentUser.pup_fully_vaccinated ? "Yes" : "No"}<br/>
+            Pup Personality: {props.currentUser.pup_personality}<br/>
+            Pup Location: {props.currentUser.pup_location}<br/>
+            Owner Name: {props.currentUser.owner_name}<br/>
+            Email: {props.currentUser.email}<br/><br/>
         </div>
     )
     
