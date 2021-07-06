77187834d7a748338c072dc718c2fcb7ff1385d5
lisellelogan <liselle.logan@yahoo.com>
2021-07-06 00:04:00 -0400

    make spaces on form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -47,19 +47,19 @@ class SignUpForm extends Component {
         return(
             <div> 
                 <form className="form" onSubmit={this.handleOnSubmit}>
-                    <label>Email: </label> <input onChange={this.handleOnChange} type="text" name="email" value={this.state.email} placeholder="email@email.com" required /><br/>
-                    <label>Password: </label> <input onChange={this.handleOnChange} type="text" name="password" value={this.state.password} required /><br/>
-                    <label>Pup Name: </label> <input onChange={this.handleOnChange} type="text"name="pupName" value={this.state.pupName} required /><br/> 
-                    {/* ask about implementing years old or months old or if i should just change data type to string in backend */}
-                    <label>Pup Age: </label> <input onChange={this.handleOnChange} type="string" name="pupAge" value={this.state.pupAge} placeholder="please specify months or years old" required /><br/> 
-                    <label>Pup Breed: </label><input onChange={this.handleOnChange} type="text" name="pupBreed" value={this.state.pupBreed} placeholder="ex. Corgi, Beagle, Terrier" required /><br/> 
+                    <label>Email: </label> <input onChange={this.handleOnChange} type="text" name="email" value={this.state.email} placeholder="email@email.com" required /><br/><br/>
+                    <label>Password: </label> <input onChange={this.handleOnChange} type="text" name="password" value={this.state.password} required /><br/><br/>
+                    <label>Pup Name: </label> <input onChange={this.handleOnChange} type="text"name="pupName" value={this.state.pupName} required /><br/> <br/>
+                    <label>Pup Age: </label> <input onChange={this.handleOnChange} type="string" name="pupAge" value={this.state.pupAge} required /><br/>
+                    <i>Please specify months or years old</i><br/><br/>
+                    <label>Pup Breed: </label><input onChange={this.handleOnChange} type="text" name="pupBreed" value={this.state.pupBreed} placeholder="ex. Corgi, Beagle, Terrier" required /><br/> <br/>
                     <label>Pup Fully Vaccinated? </label><br/>
                     Yes: <input onChange={this.handleOnChange} type="radio" name="pupFullyVaccinated" value="yes" />
-                    No: <input onChange={this.handleOnChange} type="radio" name="pupFullyVaccinated" value="no" /><br />
-                    <label>Pup Personality: </label> <input onChange={this.handleOnChange} type="text" name="pupPersonality" value={this.state.pupPersonality} required /><br/>
-                    <label>Pup Location: </label> <input onChange={this.handleOnChange} type="text" name="pupLocation" value={this.state.pupLocation} placeholder="ex. New York, NY" required /><br/>
-                    <label>Owner Name: </label> <input onChange={this.handleOnChange} type="text" name="ownerName" value={this.state.ownerName} required /><br/>
-                    <label>Profile Picture: </label><input onChange={this.handleOnChange} type="text" name="picture" value={this.state.picture} placeholder="image link" required /><br/>
+                    No: <input onChange={this.handleOnChange} type="radio" name="pupFullyVaccinated" value="no" /><br /><br/>
+                    <label>Pup Personality: </label> <input onChange={this.handleOnChange} type="text" name="pupPersonality" value={this.state.pupPersonality} required /><br/><br/>
+                    <label>Pup Location: </label> <input onChange={this.handleOnChange} type="text" name="pupLocation" value={this.state.pupLocation} placeholder="ex. New York, NY" required /><br/><br/>
+                    <label>Owner Name: </label> <input onChange={this.handleOnChange} type="text" name="ownerName" value={this.state.ownerName} required /><br/><br/>
+                    <label>Profile Picture: </label><input onChange={this.handleOnChange} type="text" name="picture" value={this.state.picture} placeholder="image link" required /><br/><br/>
                     <input type="submit" value="Signup" />
                 </form>
             </div>
