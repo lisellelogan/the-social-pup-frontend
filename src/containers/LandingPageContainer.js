e8e6ec0e86f1f3953fcd720ebb47b7803334670b
lisellelogan <liselle.logan@yahoo.com>
2021-07-05 20:02:08 -0400

    Aligned text center

--- src/containers/LandingPageContainer.js
+++ src/containers/LandingPageContainer.js
@@ -3,18 +3,20 @@ import NavBarLanding from '../components/NavBarLanding';
 import { Switch, Route } from 'react-router-dom';
 import LogInForm from '../components/LogInForm';
 import SignUpForm from '../components/SignUpForm';
+import { Button , Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class LandingPageContainer extends Component {
     render(){
         return(
-            <div>
+            <div className="landingPage">
                 <h1>Welcome to The Social Pup! We are in the landing page container!</h1>
                 <h2>Let's be friends!</h2>
                 <NavBarLanding />
-                <Switch>
-                    <Route path="/login" exact component={LogInForm} />
-                    <Route path="/signup" exact component={SignUpForm} />
-                </Switch>
+                    <Switch>
+                        <Route path="/login" exact component={LogInForm} />
+                        <Route path="/signup" exact component={SignUpForm} />
+                    </Switch>
             </div>
         )
     }
